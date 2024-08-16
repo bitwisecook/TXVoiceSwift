95fbc9469c9976d1b1a54cffee71d23e96b73687
ffm <ffm@ffm.com>
2024-08-16 11:09:00 +0100

    Initial Commit

---
+++ TaranisVoice/TaranisVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaranisVoiceApp.swift
+//  TaranisVoice
+//
+//  Created by James Deucker on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaranisVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaranisVoiceTests/TaranisVoiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaranisVoiceTests.swift
+//  TaranisVoiceTests
+//
+//  Created by James Deucker on 16/08/2024.
+//
+
+import Testing
+@testable import TaranisVoice
+
+struct TaranisVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
